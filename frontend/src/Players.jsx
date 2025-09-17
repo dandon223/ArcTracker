e4db5f025341467458984025955073d903ebe3fe
dandon223 <danielggg@onet.pl>
2025-09-17 20:46:11 +0200

    show Players: everytime

--- frontend/src/Players.jsx
+++ frontend/src/Players.jsx
@@ -46,7 +46,7 @@ export default function Players() {
     return  <section className="players">
 
         <div>
-            {players.length > 0 && <h3>Players:</h3>}
+            <h3>Players:</h3>
             <ul className="playersList">{playersList}</ul>
         </div>
         <form className="AddPlayerForm" onSubmit={handleAddPlayer}>
