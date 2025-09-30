893455ad8d1b426371ed94acbd8b3d7b147e728d
dandon223 <danielggg@onet.pl>
2025-09-30 11:39:35 +0200

    added Player.jsx

--- frontend/src/CurrentGame.jsx
+++ frontend/src/CurrentGame.jsx
@@ -2,12 +2,15 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 const API_URL = import.meta.env.VITE_API_URL;
 import Card from "./Card";
+import Player from "./Player";
 
 export default function CurrentGame() {
     const [game, setGame] = useState({});
     const [cards, setCards] = useState([]);
+    const [players, setPlayers] = useState([]);
     const cardsElements = cards.filter(card => game.cards_not_played?.includes(card.id)).map(card => <Card key={card.id} number={card.number} suit={card.suit}/>)
-
+    const playerElements = players.filter(player => game.players?.includes(player.id)).map(player => <Player key={player.id} nick={player.nick}/>)
+    
     useEffect(() => {
         fetch(`${API_URL}/api/games/?name=${encodeURIComponent(name)}`, { 
             credentials: "include",
@@ -24,12 +27,25 @@ export default function CurrentGame() {
             .then((data) => setCards(data));
       }, []);
 
+    useEffect(() => {
+        fetch(`${API_URL}/api/players/`, { 
+            credentials: "include",
+            headers:{"Accept": "application/json"} })
+            .then((res) => res.json())
+            .then((data) => setPlayers(data));
+      }, []);
+
     const { name } = useParams();
     return (
         <section className="current-game">
             <h2>CURRENT GAME: {name}</h2>
+            <h3>Cards in Play:</h3>
             <div className="cards-container">
             {cardsElements}
+            </div>
+            <h3>Players:</h3>
+            <div className="players-container">
+            {playerElements}
             </div>
         </section>
     );
